48077ebb816f187251f885e036517b9501fd797d
ferchi <fsalinas628@gmail.com>
2021-07-07 19:32:30 -0500

    sesion 7

---
+++ index.js
@@ -0,0 +1,4 @@
+//Crear proyecto de node.js
+//npm init -> Agregar la configuracion
+//npm i sass -> instalar dependencia de sass desde https://www.npmjs.com/package/sass
+//crear .gitignore -> https://www.toptal.com/developers/gitignore
